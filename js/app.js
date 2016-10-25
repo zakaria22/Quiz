f90c9fb84898f3596f828874a4c2bf6eee529f32
zakaria22 <lahyani.zakaria.lz@gmail.com>
2016-10-25 18:25:14 +0200

    list controller

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// immediatly invoking function expression
+// func that will run automaticaly when the page loads 
+(function() {
+	/**
+	 * in the module : 1. module names
+	 * 				   2. array (takes dependancies of the module)
+	 */
+	angular
+		.module("quizFacts", []);
+})();
