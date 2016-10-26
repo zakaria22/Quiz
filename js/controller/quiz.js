060ccdc3418b6090d694cdbfaea78026be74a8d2
zakaria22 <lahyani.zakaria.lz@gmail.com>
2016-10-26 13:23:37 +0200

    making a user friendly quiz choices

--- js/controller/quiz.js
+++ js/controller/quiz.js
@@ -10,5 +10,37 @@
 
 			vm.quizMetrics = quizMetrics;
 			vm.dataService = dataService;
+
+			vm.setActiveQuestion = setActiveQuestion;
+			vm.questionAnswered = questionAnswered;
+			vm.activeQuestion = 0;
+
+			var numQuestionsAnswered = 0;
+
+			function setActiveQuestion() {
+				var breakOut = false;
+				var quizLength = dataService.quizQuestions.length - 1;
+
+				while (!breakOut) {
+					vm.activeQuestion = vm.activeQuestion < quizLength ? ++vm.activeQuestion:0;
+
+					if (dataService.quizQuestions[vm.activeQuestion].selected === null) {
+						breakOut = true;
+					}
+				}
+			}
+
+			function questionAnswered(){
+				var quizLength = dataService.quizQuestions.length;
+
+				if( dataService.quizQuestions[vm.activeQuestion].selected !== null){
+					numQuestionsAnswered++;
+					if(numQuestionsAnswered >= quizLength){
+
+					}
+				}
+
+				vm.setActiveQuestion();
+			}
 		}
 })();
